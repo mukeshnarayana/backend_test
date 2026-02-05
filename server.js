b94ee02af9a95db7aae9c440ef171ad55e0a2663
mukeshnarayana <mukeshmukki900@gmail.com>
2026-02-05 16:35:12 +0530

    env error

--- server.js
+++ server.js
@@ -12,24 +12,24 @@ dotEnv.config()
 const app = express()
 const PORT = process.env.port
 app.use(express.json())
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 const swagggerDocument = yaml.load('./api.yaml')
 
-app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swagggerDocument))
-console.log(`swagger running port http://192.168.1.9:${PORT}/api-docs`)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagggerDocument))
+console.log(`swagger running port http://localhost:${PORT}/api-docs`)
 
 
-app.use('/workers',workroute)
-app.use('/menu',menurouter)
+app.use('/workers', workroute)
+app.use('/menu', menurouter)
 
 
 
 
 
-mongodb().then(()=>{
-    app.listen(PORT,()=>{
+mongodb().then(() => {
+    app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`)
     })
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err)
 })
